eb6a92d116e180d4a2ce612f0e91dde98c4e6dfa
dorianstorella <dorianstorella@gmail.com>
2023-12-12 23:59:39 +0100

    add last js for reset other onglet

--- app/index.js
+++ app/index.js
@@ -9,8 +9,21 @@ for (let index = 0; index < icon.length; index++)
 
 function openOnglet()
 {
+    let closeOtherOnglet = document.getElementsByClassName("button-icon-plus");
     
-
+    for (let index = 0; index < closeOtherOnglet.length; index++)
+    {
+        const element = closeOtherOnglet[index];
+       
+        if(element.nextElementSibling.style.display === "block")
+        {
+            element.nextElementSibling.style.display = "none";
+            element.setAttribute("src", "../faq-accordion-main/assets/images/icon-plus.svg");
+            element.setAttribute("onclick", "openOnglet()");
+        }
+        
+    }
+    
     let element = document.activeElement
     element.removeAttribute("src");
     element.setAttribute("src", "../faq-accordion-main/assets/images/icon-minus.svg");
