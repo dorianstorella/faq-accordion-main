6e3b59c98ad69e680c0a0a5d68c3a595fed814e7
Dorian Storella <dorianstorella@gmail.com>
2023-12-12 16:01:24 +0100

    remove active onglet

--- app/index.js
+++ app/index.js
@@ -1,18 +1,30 @@
  
 const icon = document.getElementsByClassName("button-icon-plus") 
 
+for (let index = 0; index < icon.length; index++) 
+{
+    const element = icon[index];
+    element.setAttribute("onclick", "openOnglet()");
+}
+
 function openOnglet()
 {
+    
+
     let element = document.activeElement
-   // console.log(element.attributes);
     element.removeAttribute("src");
     element.setAttribute("src", "../faq-accordion-main/assets/images/icon-minus.svg");
+    element.setAttribute("onclick", "closeOnglet()");
     element.nextElementSibling.style.display = "block";
 
 }
 
-for (let index = 0; index < icon.length; index++) 
+function closeOnglet()
 {
-    const element = icon[index];
-    element.addEventListener("click", openOnglet);
+    let element = document.activeElement
+    element.removeAttribute("src");
+    element.setAttribute("src", "../faq-accordion-main/assets/images/icon-plus.svg");
+    element.setAttribute("onclick", "openOnglet()");
+    element.nextElementSibling.style.display = "none";
 }
+
