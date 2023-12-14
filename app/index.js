298994c29fe91984187649c29c34dc60602e49b1
dorianstorella <dorianstorella@gmail.com>
2023-12-14 22:45:42 +0100

    lillte change for have more resemblance Small change to more resemblance to the model

--- app/index.js
+++ app/index.js
@@ -5,14 +5,9 @@ function openOnglet()
     for (let index = 0; index < closeOtherOnglet.length; index++)
     {
         const element = closeOtherOnglet[index];
-
-        if(element.parentElement.nextElementSibling.style.display === "block")
-        {
-            element.parentElement.nextElementSibling.style.display = "none";
-            element.setAttribute("src", "../faq-accordion-main/assets/images/icon-plus.svg");
-            element.setAttribute("onclick", "openOnglet()");
-        }
-        
+        element.parentElement.nextElementSibling.style.display = "none";
+        element.setAttribute("src", "../faq-accordion-main/assets/images/icon-plus.svg");
+        element.setAttribute("onclick", "openOnglet()");
     }
     
     let element = document.activeElement
